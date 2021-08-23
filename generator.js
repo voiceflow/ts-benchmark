1b946de84d7abed3367576c4528ad82d75dc75ac
Frank Gu <frank@voiceflow.com>
2021-08-22 20:28:50 -0400

    feat: json generator

--- generator.js
+++ generator.js
@@ -4,15 +4,19 @@ const fs = require('fs');
 let content = "";
 const entries = 5000000;
 const mockSessions = 500000;
-const outputFile = "./sentences.txt";
-
+const outputFile = "./sentences.json";
+const now = new Date();
 for (let i = 0; i < entries; i++) {
   if (i % 1000 == 0) {
     console.log(i);
   }
   const sentence = randomSentence({min: 1, max: 12});
   const sessionId = Math.floor(Math.random() * (mockSessions + 1));
-  content += sessionId + "," + sentence + "\n";
+  content += JSON.stringify({
+    timestamp: now.toString(),
+    sessionId,
+    sentence
+  }) + "\n";
 }
 
 try {
